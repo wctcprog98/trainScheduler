c67d1d0a8926d0cea6816b96052db7dbdacbfac1
Nenye <chinenyediei@gmail.com>
2019-07-13 18:14:00 -0500

    Initial web page setup with bootstrap

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB935yBtyBGTdfN_4_5x1Tk20DdoFM9YuI",
+    authDomain: "trainapp-83c42.firebaseapp.com",
+    databaseURL: "https://trainapp-83c42.firebaseio.com",
+    projectId: "trainapp-83c42",
+    storageBucket: "",
+    messagingSenderId: "932695329408",
+    appId: "1:932695329408:web:070d1dae4a9628af"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
