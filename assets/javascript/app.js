e54fd6e7ad0ee31df1fcb63d2da8ce4cb7ff72ad
Nenye <chinenyediei@gmail.com>
2019-07-14 19:49:20 -0500

    console is capturing values on submit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,4 +8,38 @@ var firebaseConfig = {
     appId: "1:932695329408:web:070d1dae4a9628af"
   };
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+ // Initial Values
+ var trainName = "";
+ var destination = "";
+ var firstTrain = 0;
+var frequency = 0;
+ 
+
+//submit form and capture click 
+$(".submit").on("click", function () {
+  event.preventDefault();
+
+  //capture user input 
+  trainName = $("#trainName").val().trim();
+  destination = $("#destination").val().trim();
+  firstTrain = $("#trainTime").val().trim();
+  frequency = $("#freq").val().trim();
+
+// Reference recommendations object in your Firebase database
+  var recommendations = firebase.database().ref("recommendations");
+  
+  //push data to firebase
+  recommendations.push({
+    "Train Name": trainName,
+    "Destination": destination,
+    "First Train Time": firstTrain,
+    "Frequency ": frequency
+  })
+  //append data to screen
+
+  console.log(trainName + destination + firstTrain + frequency); 
+})
