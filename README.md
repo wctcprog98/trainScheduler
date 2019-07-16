49ae677de619fa392663d394f207dca9284e455e
Nenye <chinenyediei@gmail.com>
2019-07-16 02:46:34 -0500

    finalized and working correctly displaying times and user inputs

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# trainScheduler
+
+### Train Scheduler
+
+TrainScheduler is an application that allows the user to enter information about a train and stores the information from the user in a realtime databse. The information is then sent to the webpage and can be viewed. 
+
+## How it works
+This program takes in the following user inputs.
+- Train Name
+- Destination
+- First Train Time
+- Frequency
+
+This values are stored as variables and then sent to our database to be referenced once the form has been submitted. The program will then display a table that will show
+
+- Train Name
+- Destination
+- Frequency
+- Next Arrival
+- Minutes Away
+
+We format the data using moment js to display the the arrival time. The next arrival time is computed by utilizing moment js and is formatted with the correct AM or PM time. 
+
+## Built With
+* Jquery
+* JavaScript
+* CSS
+* HTML
+* moment js
+* bootstrap
+* firebase
+
+## To Do 
+
+* Add sound
+* Add more images
+* Convert this to an API that provides realtime train information
+
+## Author 
+Nenye Diei
+
+Check out my other [projects](https://wctcprog98.github.io/responsive-portfolio/)
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -63,8 +63,34 @@ $(".submit").on("click", function () {
     console.log("Errors handled: " + errorObject.code);
   });
 
-  $("tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + trainTime + "</td><td>" + frequency + "</td><td>" + minutes + "</td></tr>");
-     console.log(trainName); 
+  var firstTime = "03:30";
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+  
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % frequency;
+  console.log(tRemainder);
+
+  // Minute Until Train
+  var tMinutesTillTrain = frequency - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  nextTrain = moment(nextTrain).format("hh:mm A");
+
+  $("tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextTrain + "</td><td>" + tMinutesTillTrain + "</td></tr>");
+  
  });
 
 
