65666f35a4b7a834131043b538510012710a8175
Nenye <chinenyediei@gmail.com>
2019-07-16 00:28:16 -0500

    user info correctly appending to screen, needs to be styled with CSS for alignment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,72 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyB935yBtyBGTdfN_4_5x1Tk20DdoFM9YuI",
-    authDomain: "trainapp-83c42.firebaseapp.com",
-    databaseURL: "https://trainapp-83c42.firebaseio.com",
-    projectId: "trainapp-83c42",
-    storageBucket: "",
-    messagingSenderId: "932695329408",
-    appId: "1:932695329408:web:070d1dae4a9628af"
-  };
-  // Initialize Firebase
+  apiKey: "AIzaSyB935yBtyBGTdfN_4_5x1Tk20DdoFM9YuI",
+  authDomain: "trainapp-83c42.firebaseapp.com",
+  databaseURL: "https://trainapp-83c42.firebaseio.com",
+  projectId: "trainapp-83c42",
+  storageBucket: "",
+  messagingSenderId: "932695329408",
+  appId: "1:932695329408:web:070d1dae4a9628af"
+};
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 var database = firebase.database();
 
- // Initial Values
- var trainName = "";
- var destination = "";
- var firstTrain = 0;
+// Initial Values
+var trainName = "";
+var destination = "";
+var firstTrain = 0;
 var frequency = 0;
- 
+var minutes = 0; 
 
-//submit form and capture click 
 $(".submit").on("click", function () {
-  event.preventDefault();
-
+  //submit form and capture click
   //capture user input 
+  event.preventDefault(); 
   trainName = $("#trainName").val().trim();
   destination = $("#destination").val().trim();
-  firstTrain = $("#trainTime").val().trim();
+  trainTime = $("#trainTime").val().trim();
   frequency = $("#freq").val().trim();
 
-// Reference recommendations object in your Firebase database
-  var recommendations = firebase.database().ref("Train Data");
-  
-  //push data to firebase
-  recommendations.push({
-    "Train Name": trainName,
-    "Destination": destination,
-    "First Train Time": firstTrain,
-    "Frequency ": frequency
-  })
-  //append data to screen
 
-  console.log(trainName + destination + firstTrain + frequency); 
-})
+
+
+  // Reference trainInformation object in your Firebase database
+  database.ref().push({
+    "trainName": trainName,
+    "destination": destination,
+    "trainTime": trainTime,
+    "frequency": frequency
+  });
+});
+    // Firebase watcher + initial loader HINT: .on("value")
+  //create new rows on click
+   // Firebase watcher .on("child_added"
+$(".submit").on("click", function () {
+  database.ref().on("child_added", function(snapshot) {
+    // storing the snapshot.val() in a variable for convenience
+    var sv = snapshot.val();
+
+    // Console.loging the last user's data
+    console.log(sv.trainName);
+    console.log(sv.destination);
+    console.log(sv.trainTime);
+    console.log(sv.frequency);
+
+    // Change the HTML to reflect
+    // $("#name-display").text(sv.trainName);
+    // $("#email-display").text(sv.destination);
+    // $("#age-display").text(sv.trainTime);
+    // $("#comment-display").text(sv.frequency);
+
+    // Handle the errors
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
+
+  $("tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + trainTime + "</td><td>" + frequency + "</td><td>" + minutes + "</td></tr>");
+     console.log(trainName); 
+ });
+
+
+
+
